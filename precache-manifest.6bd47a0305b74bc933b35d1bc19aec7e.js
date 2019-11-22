821f66af6bdc1344ea59a209076408de260dd213
Eugene Moon <jackr@Eugenes-Air.home>
2019-11-22 12:21:29 -0500

    Updates

---
+++ precache-manifest.6bd47a0305b74bc933b35d1bc19aec7e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b07eaba3e9f20e197ca76cfa8cec4d6",
+    "url": "/redditreact/index.html"
+  },
+  {
+    "revision": "a7a704198fb5904bed1c",
+    "url": "/redditreact/static/css/2.1aa0b97d.chunk.css"
+  },
+  {
+    "revision": "f9105db919a518e042bf",
+    "url": "/redditreact/static/css/main.57be8de1.chunk.css"
+  },
+  {
+    "revision": "a7a704198fb5904bed1c",
+    "url": "/redditreact/static/js/2.b33e0bd6.chunk.js"
+  },
+  {
+    "revision": "f9105db919a518e042bf",
+    "url": "/redditreact/static/js/main.83967f94.chunk.js"
+  },
+  {
+    "revision": "1f1a31125290d37fd6a9",
+    "url": "/redditreact/static/js/runtime-main.7fe472b0.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/redditreact/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/redditreact/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/redditreact/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/redditreact/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/redditreact/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
